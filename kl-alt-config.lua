2855d401c677be8c70532cbfec7537f7d5d1ca33
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2025-01-06 18:15:23 -0300

    Create kl-alt-config.lua

---
+++ kl-alt-config.lua
@@ -0,0 +1,7 @@
+_G.Kaitun = true --// false / true (set config auto farm max level)
+
+_G.Config = { UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", discord_id = "1037147029614362624" , Note = "Pc", } 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/kinglegacy"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kilavn/vandoscript/refs/heads/main/main.lua"))()
