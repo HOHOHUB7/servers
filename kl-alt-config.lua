0e759b6cb389ae057ab1a1f09dec5cc6b0ea918f
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2025-01-06 18:48:48 -0300

    Update kl-alt-config.lua

--- kl-alt-config.lua
+++ kl-alt-config.lua
@@ -1,6 +1,40 @@
-_G.Kaitun = true --// false / true (set config auto farm max level)
-
 _G.Config = { UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", discord_id = "1037147029614362624" , Note = "Pc", } 
+
+getgenv().Key = "MARU-CQ4OC-5XUTJ-HW6T-TO7FF-XYFP5"
+getgenv().id = "1037147029614362624"
+getgenv().Script_Mode = "Kaitun_Script"
+
+_G.Settings_Farm = {
+    ["Start_Farm"] = true,
+    ["Enabled_HOP"] = true,
+    ["Fruits_List"] = {
+        'RumbleRumble',
+        'FlameFlame',
+        'MagmaMagma',
+        'LightLight',
+        'GasGas',
+        'IceIce',
+        'SnowSnow',
+        'QuakeQuake',
+        'SpiritSpirit',
+        'PhoenixPhoenix',
+        'DoughDough',
+        'DragonDragon'
+    },
+    ["Enabled_Invisible"] = true,
+    ["Enabled_FastFarm"] = true,
+    ["Amount_FPS"] = 10,
+    ["LOCK_FPS"] = true,
+    ["WhiteScreen"] = true,
+    ["Disabled_Gui"] = true,
+}
+
+_G.Quests_Settings = {
+     ["V2Observation"] = false,
+     ["V2Aramament"] = false,
+     ["V2DarkLeg"] = false,
+     ["FarmRaid"] = false
+}
 
 task.spawn(function()
     pcall(function()
@@ -11,7 +45,8 @@ end)
 
 task.spawn(function()
     pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/kilavn/vandoscript/refs/heads/main/main.lua"))()
+
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
     end)
 end)
 
